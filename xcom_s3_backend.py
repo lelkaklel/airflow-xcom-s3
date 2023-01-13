f5768cffb025dac34a52cae6a41866b9844a21b4
Aleksei Solovev <lelkaklel@gmail.com>
2023-01-13 04:48:16 +0400

    conn name env variable added

--- xcom_s3_backend.py
+++ xcom_s3_backend.py
@@ -5,6 +5,7 @@ import pandas as pd
 from typing import Any
 from airflow.models.xcom import BaseXCom
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+import logging
 
 
 class S3XComBackend(BaseXCom):
@@ -20,6 +21,9 @@ class S3XComBackend(BaseXCom):
     @staticmethod
     def serialize_value(value: Any):
         if isinstance(value, pd.DataFrame):
+            logging.debug(f'connection name: {S3XComBackend.S3_XCOM_CONN_NAME}')
+            logging.debug(f'bucket name: {S3XComBackend.BUCKET_NAME}')
+            logging.debug(f'filename: {filename}')
             S3XComBackend._assert_s3_backend()
             hook = S3Hook(S3XComBackend.S3_XCOM_CONN_NAME)
             key = f"data_{str(uuid.uuid4())}.csv"
